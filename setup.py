652694650273badeccd08ec3b3ff93a1101c2caa
Stephane Bruckert <contact@stephanebruckert.com>
2019-12-13 19:28:36 +0000

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='trackfilter',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='Remove YouTube-related garbage from song titles.',
     long_description='%s\n%s' % (
