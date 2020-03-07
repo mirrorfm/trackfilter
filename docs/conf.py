b2a849a20d7ea4bf2d8a5f2e5da9c61d782e1326
Stephane Bruckert <contact@stephanebruckert.com>
2020-03-07 16:52:44 +0000

    Bump version: 0.1.3 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'TrackFilter'
 year = '2019'
 author = 'Stephane Bruckert'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.3'
+version = release = '0.2.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='trackfilter',
-    version='0.1.3',
+    version='0.2.0',
     license='MIT',
     description='Remove YouTube-related garbage from song titles.',
     long_description='%s\n%s' % (
